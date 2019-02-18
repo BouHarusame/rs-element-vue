480545c95639c6bb405c8c6cd4593572c775d59b
baoqiang <qiang.bao@roadshare.com>
2019-02-18 14:07:02 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueResource from "vue-resource";
+import "./common/stylus/index.styl";
 
 Vue.config.productionTip = false;
-
+Vue.use(VueResource);
 new Vue({
   router,
   store,
